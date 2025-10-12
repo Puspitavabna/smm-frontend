86859da7a5d63cb455b0f2cc70dfab44bbd82a18
unknown <puspitagit77@gmail.com>
2025-10-12 17:56:19 +0600

    fix error

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -13,7 +13,7 @@ export default function SigninPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch(`https://smm-backend-nine.vercel.app/api/auth/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
