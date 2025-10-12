a2c5d909eb415960e29f1cc7b9c8b900380e6002
unknown <puspitagit77@gmail.com>
2025-10-12 15:25:08 +0600

    fix error

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -13,7 +13,7 @@ export default function SigninPage() {
     e.preventDefault();
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
